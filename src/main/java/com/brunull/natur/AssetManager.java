f1f4acf8da5031ca0fcc4f89aa3e1a6cee609a5a
Bruno Camargo <bruno.br.camargo@gmail.com>
2019-05-29 20:14:18 -0300

    Cache sprites

--- src/main/java/com/brunull/natur/AssetManager.java
+++ src/main/java/com/brunull/natur/AssetManager.java
@@ -5,20 +5,33 @@ import java.awt.FontFormatException;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 
 import javax.imageio.ImageIO;
 
 import com.brunull.natur.graphics.Sprite;
 
 public class AssetManager {
+	
+	private static HashMap<String, Sprite> spriteCache = new HashMap<String, Sprite>();
+	
     public static BufferedImage loadImage(String name) throws IOException {
         BufferedImage image = ImageIO.read(AssetManager.class.getResource(name));
         return image;
     }
     
     public static Sprite loadSprite(String name) throws IOException {
+    	
+    	if (spriteCache.containsKey(name)) {
+    		return spriteCache.get(name);
+    	}
+    	
     	BufferedImage image = loadImage(name);
-    	return new Sprite(image);
+    	
+    	Sprite sprite = new Sprite(image);
+    	spriteCache.put(name, sprite);
+    	
+    	return sprite;
     }
     
     public static Font loadFont(String name) throws IOException, FontFormatException {
